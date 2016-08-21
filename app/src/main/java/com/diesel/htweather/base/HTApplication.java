b02fe67cf81ddec976b34d8f880e63b4fe005036
penley <peng416xiaoli@163.com>
2016-08-21 09:17:08 +0800

    第一次提交

--- app/src/main/java/com/diesel/htweather/base/HTApplication.java
+++ app/src/main/java/com/diesel/htweather/base/HTApplication.java
@@ -41,6 +41,8 @@ public class HTApplication extends Application {
         mInstance = this;
         CrashHandler.getInstance().init(this);
         initOkHttp();
+
+
         Fresco.initialize(this);
     }
 
