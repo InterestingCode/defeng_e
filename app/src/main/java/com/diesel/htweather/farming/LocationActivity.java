615e78056d32afac72309f9ea04a4c5b724a5a16
971596052 <971596052@qq.com>
2016-08-27 17:42:02 +0800

    配置

---
+++ app/src/main/java/com/diesel/htweather/farming/LocationActivity.java
@@ -0,0 +1,15 @@
+package com.diesel.htweather.farming;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.diesel.htweather.R;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+}
