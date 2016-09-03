a62c27d3344758ceda02e13b7d7da84dbce6f8f0
971596052 <971596052@qq.com>
2016-09-03 17:26:16 +0800

    首页

--- app/src/main/java/com/diesel/htweather/widget/Trend24HourView.java
+++ app/src/main/java/com/diesel/htweather/widget/Trend24HourView.java
@@ -4,9 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 
 import com.diesel.htweather.R;
@@ -38,8 +38,6 @@ public class Trend24HourView extends View {
 
     private Paint bgLinePaint; // 背景线 画笔
 
-    private ArrayList<Integer> xPoints = new ArrayList<>(); // 横坐标点的位置
-
     private float mCircleRadius; // 点 的半径
 
     private int width; //横坐标
@@ -48,31 +46,22 @@ public class Trend24HourView extends View {
 
     private List<Integer> temperatures = new ArrayList<>(); // 24小时温度集合
 
-//    private ArrayList<Weather24Hour> weather24Hours = null; // 24小时数据
-
     //最高温和最低温
     private int mMinTemperature;
 
     private int mMaxTemperature;
 
-    private int todayIndex = 1; // 在24小时中现在的位置
+    private int topBottomMargin = 0; // 曲线与上下边距
 
-
-    private int NowColor = getResources().getColor(R.color.common_light_blue_txt_color);
-
-    private int PreColor = Color.GRAY;
-
-    private int OtherColor = Color.WHITE;
-
-    private int topBottomMargin = 20; // 曲线与上下边距
+    private int leftRightMargin = 0; // 折线左右间距
 
     private int mTemperatureDiff = 1; // 温度差
 
+    private int timeHeight;
+
+    private int unitWidth;
+
     Context context;
-
-    String tempWeather = "";
-
-    int reduce;
 
     private int mSpace;
 
@@ -90,8 +79,9 @@ public class Trend24HourView extends View {
 
     private void init() {
         mCircleRadius = ViewUtils.dip2px(getContext(), 4);
-        reduce = context.getResources().getDimensionPixelOffset(R.dimen.trend24_weathericon_reduce);
         mSpace = ViewUtils.dip2px(getContext(), 50);
+        timeHeight = ViewUtils.dip2px(getContext(), 22.5f);
+        unitWidth = ViewUtils.dip2px(getContext(), 30);
 
         // 初始化各个画笔
         mPointPaint = new Paint();
@@ -106,7 +96,7 @@ public class Trend24HourView extends View {
 
         mTextPaint = new Paint();
         mTextPaint.setAntiAlias(true);
-        mTextPaint.setTextSize(20);
+        mTextPaint.setTextSize(ViewUtils.dip2px(getContext(), 12));
         mTextPaint.setTextAlign(Paint.Align.CENTER);
         mTextPaint.setColor(Color.WHITE);
 
@@ -115,7 +105,6 @@ public class Trend24HourView extends View {
         bgLinePaint.setStrokeWidth(1.0f);
         bgLinePaint.setStyle(Paint.Style.FILL);
         bgLinePaint.setColor(getResources().getColor(R.color.bg_white_with_90per_alpha));
-
     }
 
     /**
@@ -146,122 +135,102 @@ public class Trend24HourView extends View {
             return;
         }
 
-        for (int i = 0; i < tempSize; i++) {
-            xPoints.add((width * (2 * i + 1)) / (tempSize * 2));
-        }
+        // draw four horizontal divider line
+        int dividerLineStartX = leftRightMargin / 2 + unitWidth;
+        int dividerLineStopX = width - leftRightMargin / 2;
+        int y1 = gridSpaceHeight + topBottomMargin;//height / 4;
+        int y2 = gridSpaceHeight * 2 + topBottomMargin;//height / 2;
+        int y3 = gridSpaceHeight * 3 + topBottomMargin;//height * 3 / 4;
+        int y4 = gridSpaceHeight * 4 + topBottomMargin;//height;
+        canvas.drawLine(dividerLineStartX, y1, dividerLineStopX, y1, bgLinePaint);
+        canvas.drawLine(dividerLineStartX, y2, dividerLineStopX, y2, bgLinePaint);
+        canvas.drawLine(dividerLineStartX, y3, dividerLineStopX, y3, bgLinePaint);
+        canvas.drawLine(dividerLineStartX, y4, dividerLineStopX, y4, bgLinePaint);
 
-        if (2 * topBottomMargin > this.getHeight() / 2) { // 如果上下边距大于总宽度
-            topBottomMargin = this.height / 6;
-        }
-        int y = this.height - 2 * topBottomMargin; // 总高度减去上下边距
-        // 每一度的高度（TrendView平分为 tempFall份,再除2表示将高度单位距离减小，趋势就变缓一些）
-        int oneTempHeight = y / mTemperatureDiff / 2;
+        // draw unit value
+        int unitStartX = unitWidth / 2;
+        canvas.drawText("35", unitStartX, y1, mTextPaint);
+        canvas.drawText("33", unitStartX, y2, mTextPaint);
+        canvas.drawText("32", unitStartX, y3, mTextPaint);
+        canvas.drawText("31", unitStartX, y4, mTextPaint);
 
-        PreColor = getResources().getColor(R.color.weather_trend_forward_day_color);
+        // draw vertical divider line
+//        for (int i = 1; i < tempSize; i++) {
+//            int startY = topBottomMargin;
+//            int stopY = height - topBottomMargin - timeHeight;
+//            canvas.drawLine(gridSpaceWidth * i + unitWidth, startY, gridSpaceWidth * i + unitWidth,
+//                    stopY, bgLinePaint);
+//        }
 
-        //画四条线
-        int y1 = height / 4;
-        int y2 = height / 2;
-        int y3 = height * 3 / 4;
-        int y4 = height;
-        int lineStopX = xPoints.get(tempSize - 1) + xPoints.get(0);
-        canvas.drawLine(0, y1, lineStopX, y1, bgLinePaint);
-        canvas.drawLine(0, y2, lineStopX, y2, bgLinePaint);
-        canvas.drawLine(0, y3, lineStopX, y3, bgLinePaint);
-        canvas.drawLine(0, y4, lineStopX, y4, bgLinePaint);
-        Log.d(tag,
-                "onDraw() 画四条线 stopX(" + lineStopX + "), y1(" + y1 + "), y2(" + y2 + "), y3("
-                        + y3 + "), y4(" + y4 + ")");
-
-        int leftRightMargin = 20;
-//        int placeHeight = ViewUtils.dip2px(getContext(), 30) / 2;
-//        int perHeight = (height - placeHeight) / mTemperatureDiff;
-
-        int startX = leftRightMargin;
-        int stopX = startX + mSpace;
-        int startY = 0;
+        // draw polyline
+        int perHeight = (height - topBottomMargin * 2 - timeHeight) / mTemperatureDiff;
+        float textBottom = mTextPaint.getFontMetrics().bottom;
+        int textY = (int) (height - (timeHeight - textBottom) / 2);
+        Log.i(tag,
+                "onDraw() gridSpaceWidth=" + gridSpaceWidth + "; gridSpaceHeight=" + gridSpaceHeight
+                        + "; perHeight=" + perHeight + "; width=" + width + "; height=" + height
+                        + "; timeHeight=" + timeHeight + "; textBottom=" + textBottom + "; textY="
+                        + textY);
+        // 32, 33, 33, 35, 35, 36, 35, 37, 39, 38, 34, 33
+        int startX;
+        int startY;
+        int stopX = 0;
         int stopY = 0;
         for (int i = 0; i < tempSize; i++) {
-            if (i != tempSize - 1) {
-                startY = y - (temperatures.get(i) - mMinTemperature) * oneTempHeight
-                        - topBottomMargin / 2;
-                stopY = y - (temperatures.get(i + 1) - mMinTemperature) * oneTempHeight
-                        - topBottomMargin / 2;
+            if (i < tempSize - 1) {
+                startX = (int) (gridSpaceWidth * (i + 0.5)) + unitWidth;
+                startY = height - (temperatures.get(i) - mMinTemperature) * perHeight
+                        - topBottomMargin - timeHeight;
+                stopX = (int) (gridSpaceWidth * (i + 1.5)) + unitWidth;
+                stopY = height - (temperatures.get(i + 1) - mMinTemperature) * perHeight
+                        - topBottomMargin - timeHeight;
                 canvas.drawLine(startX, startY, stopX, stopY, mLinePaint);
             } else {
+                startX = stopX;
                 startY = stopY;
             }
+            Log.d(tag, "onDraw() start(" + startX + ", " + startY + "); stop(" + stopX + ", "
+                    + stopY + "); i=" + i + "; temp=" + temperatures.get(i));
+
+            if (i % 2 == 0) {
+                mPointPaint.setColor(
+                        ContextCompat.getColor(getContext(), R.color.polyline_yellow_point_color));
+            } else {
+                mPointPaint.setColor(
+                        ContextCompat.getColor(getContext(), R.color.polyline_blue_point_color));
+            }
             canvas.drawCircle(startX, startY, mCircleRadius, mPointPaint);
-            startX = stopX;
-            stopX = startX + mSpace;
+            int valueStartY = startY - 25;
+            if (startY > 0 && startY < 50) {
+                valueStartY = startY + 50;
+            }
+            canvas.drawText(temperatures.get(i)+"°", startX, valueStartY, mTextPaint);
+
+//            float textStartY = height - (timeHeight - mTextPaint.getFontMetrics().bottom) / 2;
+//            float textStartY = height - timeHeight + (timeHeight - mTextPaint.getFontMetrics().bottom);
+            float textStartY = height - 20;
+            canvas.drawText("11:0" + i, startX, textStartY, mTextPaint);
+            Log.i(tag, "onDraw()  draw text  startX=" + startX + "; textStartY=" + textStartY);
         }
 
-//        for (int i = 0; i < tempSize; i++) {
-//            if (i != tempSize - 1) {
-//                if (temperatures.get(i + 1) != 100
-//                        && temperatures.get(i) != 100) { // 若温度值不为100(异常)，异常数据不划线
-//
-//                    if (i + 1 <= todayIndex) {
-//                        mLinePaint.setColor(PreColor);
-//                    } else {
-//                        mLinePaint.setColor(OtherColor);
-//                    }
-//
-//                    // 连接两点画折线
-//                    int startY = y - ((temperatures.get(i) - mMinTemperature) * oneTempHeight)
-//                            - topBottomMargin / 2; // 起点 总高度 - 度数*每度高度 + 上边距
-//                    int endY = y - ((temperatures.get(i + 1) - mMinTemperature) * oneTempHeight)
-//                            - topBottomMargin / 2;
-//                    canvas.drawLine(
-//                            xPoints.get(i), startY,
-//                            xPoints.get(i + 1), endY,
-//                            mLinePaint);
-//
-//                } else if (temperatures.get(i) != 100) {
-//
-//                    canvas.drawCircle(xPoints.get(i), y
-//                            - ((temperatures.get(i) - mMinTemperature) * oneTempHeight)
-//                            - topBottomMargin / 2, mCircleRadius, mPointPaint);
-//
-//                    continue;
-//                }
-//            }
-//
-//            canvas.drawCircle(xPoints.get(i), y
-//                            - ((temperatures.get(i) - mMinTemperature) * oneTempHeight)
-//                            - topBottomMargin / 2,
-//                    mCircleRadius, mPointPaint);
-//        }
     }
 
     //网格的宽度与高度
-    private int gridspace_width;
+    private int gridSpaceWidth;
 
-    private int gridspace_heigh;
-
-    //底部空白的高度
-    private int brokenline_bottom;
-
-    private int heigh;
+    private int gridSpaceHeight;
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        gridspace_width = mSpace;
-        if (temperatures.size() == 0) {
-            //通过调用onMeasure源码中的方法进行获得宽度，
-            //宽度的获得有三种模式，具体介绍见博客底部连接
-            width = getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec);
-        } else {
-            //根据数据条数设置宽度
-            width = gridspace_width * temperatures.size() + 10;
-        }
+        gridSpaceWidth = mSpace;
+        width = temperatures.isEmpty() ? getDefaultSize(getSuggestedMinimumWidth(),
+                widthMeasureSpec)
+                : gridSpaceWidth * temperatures.size() + leftRightMargin * 2 + unitWidth;
+        height = getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec);
 
-        heigh = getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec);
-        //设置底部白色空白的宽度
-        brokenline_bottom = 50;
+        gridSpaceHeight = (height - topBottomMargin * 2 - timeHeight) / 4;
 
-        gridspace_heigh = (heigh - brokenline_bottom) / 4;
-        setMeasuredDimension(width, heigh);
+        setMeasuredDimension(width, height);
     }
 }
