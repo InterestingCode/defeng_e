bca75b4ec96398e3f5fa493bc39d65a791f227f4
zhoujiangsen@126.com <zhoujiangsen@126.com>
2016-10-12 14:48:07 +0800

    优化深度服务界面

--- app/src/main/java/com/diesel/htweather/DepthServiceFragment.java
+++ app/src/main/java/com/diesel/htweather/DepthServiceFragment.java
@@ -3,10 +3,11 @@ package com.diesel.htweather;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ScrollView;
 
 import com.diesel.htweather.base.BaseFragment;
 import com.diesel.htweather.depthservice.AgricultureDoctorActivity;
@@ -16,12 +17,10 @@ import com.diesel.htweather.depthservice.FarmingAdviceActivity;
 import com.diesel.htweather.depthservice.GrowthDiaryActivity;
 import com.diesel.htweather.depthservice.GrowthDiaryDetailsActivity;
 import com.diesel.htweather.depthservice.OnlineAdvisoryActivity;
-import com.diesel.htweather.depthservice.OnlineAdvisoryDetailsActivity;
 import com.diesel.htweather.depthservice.ProfileActivity;
 import com.diesel.htweather.depthservice.SettingFacilitiesActivity;
 import com.diesel.htweather.depthservice.adapter.DepthDiaryAdapter;
 import com.diesel.htweather.depthservice.model.GrowthDiaryBean;
-import com.diesel.htweather.util.ActivityNav;
 import com.diesel.htweather.widget.FullListView;
 
 import java.util.ArrayList;
@@ -47,14 +46,20 @@ public class DepthServiceFragment extends BaseFragment {
     @BindView(R.id.diaryList)
     FullListView diaryList;
 
+    @BindView(R.id.btnProfile)
+    FrameLayout mBtnProfile;
+
+    @BindView(R.id.mScrollView)
+    ScrollView mScrollView;
+
+
     public static DepthServiceFragment newInstance() {
         return new DepthServiceFragment();
     }
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_depth_service, container, false);
         ButterKnife.bind(this, view);
         initData();
@@ -70,13 +75,16 @@ public class DepthServiceFragment extends BaseFragment {
         diaryList.setAdapter(new DepthDiaryAdapter(mActivity, data));
     }
 
-    @OnClick({R.id.tvDepthFacilities, R.id.tvAgriculture, R.id.btnDepthDiary, R.id.btnJust,
-            R.id.btnAsk, R.id.btnFarming, R.id.btnProfile})
+    @OnClick({R.id.tvDepthFacilities, R.id.tvAgriculturalFacilities, R.id.tvAgriculture, R.id.btnDepthDiary, R.id.btnJust,
+            R.id.btnAsk, R.id.btnFarming, R.id.btnProfile, R.id.tvDepthClose, R.id.tvReturnTop})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.tvDepthFacilities:
                 Intent intent = new Intent(mActivity, FacilitiesActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.tvAgriculturalFacilities:
+                startActivity(new Intent(mActivity, FacilitiesDetailsActivity.class));
                 break;
             case R.id.tvAgriculture:
                 startActivity(new Intent(mActivity, AgricultureDoctorActivity.class));
@@ -95,6 +103,16 @@ public class DepthServiceFragment extends BaseFragment {
                 break;
             case R.id.btnProfile:
                 startActivity(new Intent(mActivity, ProfileActivity.class));
+                break;
+            case R.id.tvDepthClose:
+                mBtnProfile.setVisibility(View.GONE);
+                break;
+            case R.id.tvReturnTop:
+                mScrollView.fullScroll(ScrollView.FOCUS_UP);
+                break;
+
+
+            default:
                 break;
         }
     }
